5d34b4ab93e310c9085a56f33e292e806908aa20
tek62441 <tek62441@tek62441s-MacBook-Pro.local>
2018-08-30 15:09:49 -0700

    Return response and payload

--- uptimeMonitor/app.js
+++ uptimeMonitor/app.js
@@ -36,16 +36,67 @@ const server = http.createServer((req, res) => {
 	req.on('data', data => {
 		buffer += decoder.write(data);
 	});
+
 	// Send the response
 	req.on('end', () => {
 		buffer += decoder.end();
-		res.end("Hello, World" + '\n');
+
+		// Choose the handler this request should go to. If one is not found, use notFound handler.
+		const chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+		// Construct the data object to send to the handler
+		const data = {
+			'trimmedPath' : trimmedPath,
+			'queryStringObject' : queryStringObject,
+			'method' : method,
+			'headers' : headers,
+			'payload' : buffer
+		};
+
+		// Route the request to the handler specified in the router
+
+		chosenHandler(data, (statusCode, payload) => {
+			// Use the status code called back by the handler, or default to 200
+			statusCode = typeof(statusCode) === 'number' ? statusCode : 200;
+			// Use the payload called back by the handler, or default to an empty object
+			payload = typeof(payload) === 'object' ? payload : {};
+
+			// Convert the payload to a string
+			const payloadString = JSON.stringify(payload);
+
+			// Return the response
+			res.writeHead(statusCode);
+			res.end(payloadString);
+
+			console.log("Returning this response: ", statusCode, payloadString);
+		});
+
+		
+
+		// Log the payload
 		console.log('Request received with this payload: ', buffer);
 	})
 
-	// Log the request headers
 });
 
 server.listen(3000, () => {
 	console.log("The server is listening on port 3000 now");
 });
+
+// Define the handlers
+const handlers = {};
+
+// Sample handler
+handlers.sample = (data, callback) => {
+	// Callback an http status code, and a payload object
+	callback(406, {'name': 'sample handler'});
+};
+
+// Not found handler
+handlers.notFound = (data, callback) => {
+	callback(404);
+};
+// Define a request router
+const router = {
+	'sample' : handlers.sample
+};
