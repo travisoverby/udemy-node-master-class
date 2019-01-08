c9587f3009e9bd0a1978e67169ab034eea6404b4
Travis Overby <travisoverby@gmail.com>
2019-01-07 21:06:01 -0800

    Build tokens service; TODO: add user authentication

--- uptimeMonitor/lib/handlers.js
+++ uptimeMonitor/lib/handlers.js
@@ -179,6 +179,169 @@ handlers._users.delete = (data, callback) => {
   }
 };
 
+
+// Tokens
+handlers.tokens = (data, callback) => {
+  const acceptableMethods = ['post', 'get', 'put', 'delete'];
+  if (acceptableMethods.includes(data.method)) {
+    handlers._tokens[data.method](data, callback);
+  } else {
+    callback(405 + "method not allowed");
+  }
+};
+
+// Container for all the tokens methods
+handlers._tokens = {};
+
+// Tokens - post
+// Required data: phone, password
+// Optional data: none
+handlers._tokens.post = (data, callback) => {
+  const phone = typeof(data.payload.phone) === 'string' && data.payload.phone.trim().length === 10 ? data.payload.phone.trim() : false;
+  const password = typeof(data.payload.password) === 'string' && data.payload.password.trim().length > 0 ? data.payload.password.trim() : false;
+
+  if (phone && password) {
+    // Lookup the user who matches that phone number
+    _data.read('users', phone, (err, userData) => {
+      if (!err && userData) {
+        // Hash the send password, and compare it to the password stored in the user object
+        const hashedPassword = helpers.hash(password);
+        if (hashedPassword === userData.hashedPassword) {
+          // If valid, create a new token with a random name. Set expiration date one hour in the future.
+          const tokenId = helpers.createRandomString(20);
+          const expires = Date.now() + (1000 * 60 * 60);
+
+          const tokenObject = {
+            'phone' : phone,
+            'id' : tokenId,
+            'expires' : expires
+          };
+
+          // Store the token
+          _data.create('tokens', tokenId, tokenObject, err => {
+            if (!err) {
+              callback(200, tokenObject);
+            } else {
+              callback(500, {'Error' : 'Could not create the new token'});
+            }
+          });
+
+        } else {
+          callback(400, {'Error' : "Password did not match the specified user's stored password"});
+        }
+      } else {
+        callback(400, {'Error' : 'Could not find the specified user'});
+      }
+    });
+  } else {
+    callback(400, {'Error' : 'Missing required field(s)'});
+  }
+
+};
+
+// Tokens - get
+// Required data: id
+// Optional data: none
+handlers._tokens.get = (data, callback) => {
+  // Check that the id sent is valid
+  const id = typeof(data.queryStringObject.id) === 'string' && data.queryStringObject.id.trim().length === 20 ? data.queryStringObject.id.trim() : false;
+
+  if (id) {
+    // Lookup the token
+    _data.read('tokens', id, (err, tokenData) => {
+      if (!err && tokenData) {
+        // Remove the hashed password from the token object before returning it to the requestor
+        delete tokenData.hashedPassword;
+        callback(200, tokenData);
+      } else {
+        callback(404, {'Error' : "Token does not exist"});
+      }
+    });
+  } else {
+    callback(400, {'Error' : "Missing required field"});
+  }
+};
+
+// Tokens - put
+// Required fields: id, extend
+// Optional data: none
+handlers._tokens.put = (data, callback) => {
+  const id = typeof(data.payload.id) === 'string' && data.payload.id.trim().length === 20 ? data.payload.id.trim() : false;
+  const extend = typeof(data.payload.extend) === 'boolean' && data.payload.extend === true ? data.payload.extend : false;
+
+  if (id && extend) {
+    // Lookup the token
+    _data.read('tokens', id, (err, tokenData) => {
+      if (!err && tokenData) {
+        // Check to make sure the token isn't already expired
+        if (tokenData.expires > Date.now()) {
+          // Set the expiration an hour from  now
+          tokenData.expires = Date.now() + 1000 * 60 * 60;
+
+          // Store the new updates
+          _data.update('tokens', id, tokenData, err => {
+            if (!err) {
+              callback(200);
+            } else {
+              callback(500, {'Error' : 'Could not update the token\'s expiration'});
+            }
+          });
+        } else {
+          callback(400, {'Error' : 'The token has already expired and cannot be extended'});
+        }
+      } else {
+        callback(400, {'Error' : 'Specified token does not exist'});
+      }
+    });
+  } else {
+    callback(400, {'Error' : 'Missing required field(s) or field(s) are invalid'});
+  }
+};
+
+// Tokens - delete
+// Required data: id
+// Optional data: none
+handlers._tokens.delete = (data, callback) => {
+  // Check that the id is valid
+  const id = typeof(data.queryStringObject.id) === 'string' && data.queryStringObject.id.trim().length === 20 ? data.queryStringObject.id.trim() : false;
+
+  if (id) {
+    // Look up the user  
+    _data.read('tokens', id, (err, data) => {
+      if (!err && data) {
+        _data.delete('tokens', id, err => {
+          if (!err) {
+            callback(200, {'Response' : 'Token was deleted'});
+          } else {
+            callback(500, {'Error' : 'Could not delete the specified token'});
+          }
+        });
+      } else {
+        callback(400, {'Error' : "Could not find the specified token"});
+      }
+    });
+  } else {
+    callback(400, {'Error' : "Missing required field"});
+  }
+};
+
+// Verify if a given token id is currently valid for a given user
+handlers._tokens.verifyToken = (id, phone, callback) => {
+  // Lookup the token
+  _data.read('tokens', id, (err, tokenData) => {
+    if (!err && tokenData) {
+      // Check that the token is for the given user and has not expired
+      if (tokenData.phone === phone && tokenData.expires > Date.now()) {
+        callback(true);
+      } else {
+        callback(false);
+      }
+    } else {
+      callback(false);
+    }
+  });
+};
+
 // Ping handler
 handlers.ping = (data, callback) => {
   // Callback an http status code, and a payload object
--- uptimeMonitor/lib/helpers.js
+++ uptimeMonitor/lib/helpers.js
@@ -32,5 +32,31 @@ helpers.parseJsonToObject = str => {
 	}
 };
 
+// Create a string of random alphanumeric characters, of a given length
+helpers.createRandomString = strLength => {
+	strLength = typeof(strLength) === 'number' && strLength > 0 ? strLength : false;
+
+	if (strLength) {
+		// Define all the possible characters that could go into a string
+		const possibleChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+		// Start the final string
+		let str = '';
+		for (let i = 0; i < strLength; i++) {
+			// Get a random character from the possibleCharacters string
+			const randomChar = possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
+
+			// Append this char to the final string
+			str += randomChar;
+		}
+
+		// Return the final string
+		return str;
+
+	} else {
+		return false;
+	}
+};
+
 // Export the module
 module.exports = helpers;
